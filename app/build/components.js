83e0c933cd24e36e4ed53b7f7bbb331d3445c8fb
Akash Kava <support@neurospeech.com>
2017-09-07 00:40:26 +0530

    Work in progress

--- app/build/components.js
+++ app/build/components.js
@@ -10,16 +10,17 @@
   "div",
   {},
   [
-    "input"
+    "input",
+    {
+      "placeholder": "Task...",
+      "data-atom-init": "TaskList_t1"
+    }
   ],
   [
     "button",
     {
-      "atom-event-click": "atom-event-click",
-      "{": "{",
-      "()": "()",
-      "-": "-",
-      "text": " ($viewModel).addTask() }\"Add"
+      "data-atom-init": "TaskList_t2",
+      "text": "Add"
     }
   ]
 ],
@@ -27,7 +28,7 @@
   "div",
   {
     "atom-type": "AtomItemsControl",
-    "data-atom-init": "TaskList_t1"
+    "data-atom-init": "TaskList_t3"
   }
 ]];
 
@@ -36,6 +37,12 @@
                         this.setLocalValue('viewModel',new TaskListViewModel,e);
                     };
 		this.TaskList_t1 = function(e) { 
+                        this.bind(e,'value', ["viewModel","newTask"], 1 );
+                    };
+		this.TaskList_t2 = function(e) { 
+                        this.setLocalValue('eventClick',function(){ return  (Atom.get(this,"viewModel")).addTask(); },e);
+                    };
+		this.TaskList_t3 = function(e) { 
                         this.setLocalValue('items',Atom.get(this,"viewModel.items"),e);
                     };
                 }).call(WebAtoms.PageSetup,window,WebAtoms);
